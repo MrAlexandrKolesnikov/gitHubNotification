e5a701aeffcb2a00269031ae8a3b4735d9a254e7
Alexandr Kolesnikov <kolesnikova@truxan.by>
2018-04-17 14:50:20 +0300

    add while true

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ branch = "master"
 
 
 def worker():
-    if number_of_commit != Git.number_of_commit(branch):
-        notification = nf()
-        notification.rise(title='New commit',
+    while True:
+        if number_of_commit != Git.number_of_commit(branch):
+            notification = nf()
+            notification.rise(title='New commit',
                           subtitle=Git.get_last_commit_author(branch),
                           message=Git.get_last_commit_message(branch))
-    time.sleep(30)
+        time.sleep(30)
 
 
 if __name__ == '__main__':
